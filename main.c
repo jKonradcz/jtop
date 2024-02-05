5258b74c1faaeec3a4898391efec5bd76ca9390e
jKonradcz <jKonradcz@gmail.com>
2024-02-05 22:10:20 +0100

    pseudo code for the /proc list, again, this time on correct file

--- main.c
+++ main.c
@@ -1,9 +1,24 @@
 #include <stdlib.h>
 
-int main():
-    /// execute a bash command
-    char command = "ls -l";
+int main() {
+    // grab the list of files in /proc
+    system("ls -l /proc > list-new.txt");
 
-    system(command);
+    // open new output file
+
+    // allocate mem for file of text in file
+
+    // open the list 
+
+    // loop through the list
+
+        // check each line if name contains number
+
+        // enter each folder, read the cmdline, skip if empty
+
+        // write the pid + cmdline into the output file
+
+    // close the list, rename it to list-old.txt
 
     return 0; 
+}
